ace097a330fdb9d200a3566435bc168decd6c047
Rebecca Lee <rvl26@cornell.edu>
2018-06-30 02:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,23 +3,13 @@ function shout(string) {
   return string.toUpperCase ()
 }
 
-'HELLO'.toLowerCase()
+'HELLO'.toLowerCase();
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
+"hello".toUpperCase();
 function logShout(string) {
-  console.log(string)
-  spyOn(console, 'log').andCallThrough()
+  console.log(string);
+  spyOn(console, 'log').andCallThrough();
 }
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
