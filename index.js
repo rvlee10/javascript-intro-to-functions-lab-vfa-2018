f2839c5d1587468b63570a0426f9d9dde7ae9572
Rebecca Lee <rvl26@cornell.edu>
2018-06-30 02:00:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  
+}
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
